b768901b6e640e433d15df25fb80ea4b171f19ab
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2019-03-17 20:18:12 +0530

    new users adding to database

--- add_user.php
+++ add_user.php
@@ -11,8 +11,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-session_start();
-
 $user_title = $_POST["title"];
 $user_first_name = $_POST["first_name"];
 $user_second_name = $_POST["second_name"];
@@ -22,14 +20,21 @@ $user_username = $_POST["username"];
 $user_password = md5($_POST["password"]);
 $user_re_password = md5($_POST["re_password"]);
 
-if (($user_first_name != "")&&($user_second_name != "")) {
-	$query_user = "INSERT INTO users(title,first_name,second_name,email,username,password,re_password,user_type) VALUES('$user_title','$user_first_name','$user_second_name','$user_email','$user_username','$user_password','$user_re_password','$user_userrole')";
+if (($user_first_name != "")&&($user_second_name != "")&&($user_email != "")&&($user_username != "")&&($user_password != "")&&($user_re_password != "")) {
 
-	if ($conn->query($query_user)) {
-		echo "User added successfully.";
+	if ($user_password == $user_re_password) {
+		$query_user = "INSERT INTO users(title,first_name,second_name,email,username,password,re_password,user_type) VALUES('$user_title','$user_first_name','$user_second_name','$user_email','$user_username','$user_password','$user_re_password','$user_userrole')";
+
+		if ($conn->query($query_user)) {
+			echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-check-circle fa-lg"></i>
+			<b>User added successfully.</b></div>';
+		}else{
+			echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><b>User adding failed! Check your internet connection.</b></div>';
+		}
 	}else{
-		echo "User adding failed! Try again.";
+		echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><b>Entered passwords are different! Please re-enter.</b></div>';
 	}
 }else{
-	echo "Oops! Database connection lost! Check your internet connection.";
+	echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><b>All fields are required!</b></div>';
 }
